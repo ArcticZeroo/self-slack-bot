8b9cdcdced88b5f1f01046d5272f2f9acfa9e372
Spencer <spencer@artix.cc>
2017-06-30 22:46:13 -0400

    Whoops, typo in ImagePostCommands!

--- commands/ImagePostCommands.js
+++ commands/ImagePostCommands.js
@@ -15,7 +15,7 @@ function getImagePostCommand(name, image, text, aliases = []) {
                 // Edit the message to have the URL, for those who can't or won't unfurl
                 .edit(this.image)
                 // Then, return the Promise for unfurling the same image, so that it'll be caught by the same 'catch'
-                .then(r => bot.api.methods.chat.unfurl({channel: msg.channel, ts: message.ts, unfurls: { [this.image]: {text} }}))
+                .then(r => bot.api.methods.chat.unfurl({channel: msg.channel, ts: msg.ts, unfurls: { [this.image]: {text} }}))
                 // Finally, call msg.error if something goes wrong. I think that exists.
                 .catch(msg.error.bind(msg));
         }
